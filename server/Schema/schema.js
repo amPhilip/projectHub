db62b9725ad33ad5084077f3db440777bd517e79
amPhilip <philipmwangi91@gmail.com>
2022-06-13 04:12:25 +0300

    mngt-syst:adding client mutation

--- server/Schema/schema.js
+++ server/Schema/schema.js
@@ -7,6 +7,7 @@ const {
   GraphQLString,
   GraphQLSchema,
   GraphQLList,
+  GraphQLNonNull
 } = require("graphql");
 
 //Project Type
@@ -78,6 +79,38 @@ const RootQuery = new GraphQLObjectType({
   },
 });
 
+
+//MUTATIONS
+const mutation = new GraphQLObjectType({
+    name:'Mutation',
+    fields:{
+        //ADD CLIENT
+        addClient:{
+            type:ClientType,
+            args:{
+                name:{type: GraphQLNonNull(GraphQLString)},
+                email:{type: GraphQLNonNull(GraphQLString)},
+                phone:{type: GraphQLNonNull(GraphQLString)},
+            },
+            resolve(parent,args){
+                const client = new Client({
+                    name:args.name,
+                    email:args.email,
+                    phone:args.phone,
+                });
+                return client.save();
+            }
+        }
+    }
+})
+
+
+
+
+
+
+
 module.exports = new GraphQLSchema({
   query: RootQuery,
+  mutation
 });
